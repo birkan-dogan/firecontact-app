74a22507800d69446a633427ba19fc21dc227d03
birkan <b.561birkan@gmail.com>
2022-07-29 14:05:29 +0300

    adding mui library to style ,creating initialValue

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 import Form from "./components/form/Form";
 import Navbar from "./components/navbar/Navbar";
 import Table from "./components/table/Table";
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
+
+const initialValues = {
+  username: "",
+  phoneNumber: "",
+  gender: "",
+};
 function App() {
+  const [info, setInfo] = useState(initialValues);
   return (
     <>
+      <ToastContainer />
       <Navbar />
-      <div>
-        <Form />
+      <div className="App">
+        <Form info={info} setInfo={setInfo} />
         <Table />
       </div>
     </>
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,87 @@
 import React from "react";
+import {
+  Grid,
+  FormControl,
+  InputLabel,
+  TextField,
+  InputAdornment,
+  MenuItem,
+  Button,
+  Stack,
+  Select,
+  Box,
+} from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ info, setInfo }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+  return (
+    <Grid
+      textAlign="center"
+      verticalAlign="middle"
+      direction="column"
+      style={{ width: "300" }}
+    >
+      <h2 className="contact-header">Add Contact</h2>
+
+      <Box style={{ backgroundColor: "white", padding: "20px" }}>
+        <form>
+          <Stack spacing={3} direction="column">
+            <TextField
+              variant="outlined"
+              name="username"
+              value={info.username}
+              onChange={handleChange}
+              placeholder="Name"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              variant="outlined"
+              name="phoneNumber"
+              value={info.phoneNumber}
+              onChange={handleChange}
+              placeholder="Phone Number"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PhoneEnabledIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel style={{ paddingLeft: "20px" }}>Gender</InputLabel>
+              <Select
+                label="Gender"
+                name="gender"
+                variant="outlined"
+                value={info.gender}
+                onChange={handleChange}
+              >
+                <MenuItem value="Female">Female</MenuItem>
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Other">Other</MenuItem>
+              </Select>
+            </FormControl>
+            <Button variant="contained" type="submit" value="Submit">
+              ADD
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Grid>
+  );
 };
 
 export default Form;
